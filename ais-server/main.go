b9e9b030b0c8c2040f9b1de11c83bafcd0716426
Joseph Rose <jrose@librato.com>
2017-12-13 21:38:22 -0500

    Adding a simple server to deliver test positions

---
+++ ais-server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"log"
+	"bufio"
+	"time"
+	"math/rand"
+	"fmt"
+	"net"
+)
+
+var (
+	positionFile = flag.String("f", "", "The name of the file to read")
+	maxPositionSleep = flag.Int("s", 10, "The max number of seconds to sleep between positions")
+	positionChan = make(chan string)
+)
+
+func main() {
+	flag.Parse()
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	file, err := os.Open(*positionFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	positionScanner := bufio.NewScanner(file)
+	go func() {
+		for positionScanner.Scan() {
+			positionChan <- positionScanner.Text()
+			time.Sleep(time.Duration(rand.Intn(*maxPositionSleep) * int(time.Second.Nanoseconds())))
+		}
+	}()
+
+	fmt.Println("Launching server...")
+	ln, _ := net.Listen("tcp4", "127.0.0.1:10110")
+	conn, _ := ln.Accept()
+	for {
+		select {
+		case position := <- positionChan:
+			fmt.Println(position)
+			conn.Write([]byte(position))
+		}
+	}
+}
