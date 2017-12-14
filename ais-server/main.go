a3949c1cf14afeaece4af74be239250fc301eb4b
Joseph Rose <jrose@librato.com>
2017-12-13 21:45:47 -0500

    Removing binary, tweaks

--- ais-server/main.go
+++ ais-server/main.go
@@ -12,9 +12,9 @@ import (
 )
 
 var (
-	positionFile = flag.String("f", "", "The name of the file to read")
+	positionFile     = flag.String("f", "", "The name of the file to read")
 	maxPositionSleep = flag.Int("s", 10, "The max number of seconds to sleep between positions")
-	positionChan = make(chan string)
+	positionChan     = make(chan string)
 )
 
 func main() {
@@ -38,11 +38,14 @@ func main() {
 	fmt.Println("Launching server...")
 	ln, _ := net.Listen("tcp4", "127.0.0.1:10110")
 	conn, _ := ln.Accept()
+	defer conn.Close()
+
 	for {
 		select {
-		case position := <- positionChan:
+		case position := <-positionChan:
 			fmt.Println(position)
 			conn.Write([]byte(position))
+			conn.Write([]byte("\n"))
 		}
 	}
 }
